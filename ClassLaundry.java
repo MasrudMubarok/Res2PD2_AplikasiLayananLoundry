96e7889519527e43acb56397745a1b19412fdc84
MasrudMubarok <44463402+MasrudMubarok@users.noreply.github.com>
2018-11-28 05:04:30 +0700

    Add files via upload

---
+++ ClassLaundry.java
@@ -0,0 +1,17 @@
+
+package program_laudry;
+
+public class ClassLaundry {
+    public String namaPelanggan;
+    public String jenisCucian;
+    public String Kategori;
+    public String Harga;
+    public float harga;
+       
+       
+       
+       public String toSstring(){
+           return namaPelanggan+ " - " + jenisCucian+ " - "+Kategori+" - "+Harga ;
+           
+       }
+}
